a2eb28f095fcbf9ceef11a2fa3075ad6fef12ba4
yhong4623 <yhong4623@gmail.com>
2026-03-29 15:09:35 +0800

    Refactor: remove magic numbers and rename variables for better readabillity

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -1,22 +1,25 @@
-import random
+from random import uniform
 
 # Constant settings
 RADIUS = 1
+LOWER_BOUND = -RADIUS
+UPPER_BOUND = RADIUS
+
 NUM_POINTS = 1000000
 AREA_FACTOR = 4
 SQUARE_EXPONENT = 2
 POINT_COUNT_INCREMENT = 1
 
-inside_circle = 0
+inside_circle_count = 0
 
 # Randomly generate points and count those inside the circle
 for _ in range(NUM_POINTS):
-    x = random.uniform(-RADIUS, RADIUS)
-    y = random.uniform(-1, 1)
-    if x**SQUARE_EXPONENT + y**2 <= RADIUS**SQUARE_EXPONENT:
-        inside_circle += POINT_COUNT_INCREMENT
+    x = uniform(LOWER_BOUND, UPPER_BOUND)
+    y = uniform(LOWER_BOUND, UPPER_BOUND)
+    if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
+        inside_circle_count += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-pi_neapple = (inside_circle / NUM_POINTS) * AREA_FACTOR
+estimated_pi = (inside_circle_count / NUM_POINTS) * AREA_FACTOR
 
-print(f"Estimated value of pi is: {pi_neapple}")
+print(f"Estimated value of pi is: {estimated_pi}")
